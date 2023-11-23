d95dda40ea7d2f5ff535ada14d9b501c61adcb64
choorywey <123738595+choorywey@users.noreply.github.com>
2023-11-23 17:50:43 +0100

    feat(paginadisco): album post working

--- src/app/componenti/pagina-disco/pagina-disco.component.ts
+++ src/app/componenti/pagina-disco/pagina-disco.component.ts
@@ -7,7 +7,6 @@ import { tap, filter } from 'rxjs/operators';
 import { TabComponent } from '../tab/tab.component';
 import { FirebaseService } from 'src/app/servizi/firebase.service';
 import { switchMap } from 'rxjs/operators';
-
 @Component({
   selector: 'app-pagina-disco',
   templateUrl: './pagina-disco.component.html',
@@ -35,37 +34,8 @@ export class PaginaDiscoComponent implements OnInit {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log("Album ID:", this.id);
      
-    const albumId = this.id !== null ? parseInt(this.id, 10) : null;
-  
-    if (albumId !== null && !isNaN(albumId)) {
-      this.firebaseService.getAlbums().pipe(
-        switchMap((data: any) => {
-          this.albumList = data;
-          console.log(this.albumList);
-          
-          // Filter the album list to include only the one with the specified ID
-          const selectedAlbum = this.albumList.find(album => album.id === albumId);
-  
-          // Use the type assertion to tell TypeScript that albumId is not null
-          return selectedAlbum ? this.servizio.getAlbumInfoById(albumId.toString() as string) : [];
-        })
-      ).subscribe(albumData => {
-        console.log("Album Data:", albumData);
-  
-        if (albumData) {
-          this.title = albumData.title;
-          console.log(this.title);
-          this.artist = albumData.artist;
-          this.genre = albumData.genre;
-          this.coverUrl = albumData.coverUrl;
-        } else {
-          console.error("Dati dell'album non validi o mancanti.");
-        }
-      }, error => {
-        console.error("Error fetching album information:", error);
-      });
-    } else {
-      console.error("L'ID dell'album non è un numero valido.");
-    }
+    this.firebaseService.getAlbums().subscribe((data: any) => {
+      this.albumList = data;
+    })
 }
 }
