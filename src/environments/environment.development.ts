e27c9332ee7e128f4b25ec2ae0d1c1c2f3ad05ba
chiaraferrara <123738595+choorywey@users.noreply.github.com>
2024-03-10 16:23:09 +0100

    feat: update of firebase firestore environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,12 @@
 export const environment = {
-    production:false,
-    firebaseConfig : {
-      apiKey: "AIzaSyAgsPf8ri_L-QTFX6MJOy_IHl_SHw0FG3c",
-  authDomain: "vinili-165f8.firebaseapp.com",
-  projectId: "vinili-165f8",
-  storageBucket: "vinili-165f8.appspot.com",
-  messagingSenderId: "1093712895302",
-  appId: "1:1093712895302:web:4c2b2847422aa2a997d947"
-      }
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD0AihFBlf0fes6Zmri5QZScIXEp2aG_a0",
+    authDomain: "vinili-f550a.firebaseapp.com",
+    projectId: "vinili-f550a",
+    storageBucket: "vinili-f550a.appspot.com",
+    messagingSenderId: "562205585128",
+    appId: "1:562205585128:web:ef21d950fa53035c18635d",
+    measurementId: "G-DBWKHVQ9SG",
+  },
 };
